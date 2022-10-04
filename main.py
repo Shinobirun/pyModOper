c38965e85be6db3dd23f17ac346dfc3c9032cb78
Shinobirun <adrian.latorre@gmail.com>
2022-10-04 14:11:06 -0300

    final

---
+++ main.py
@@ -0,0 +1,16 @@
+import operaciones as op
+
+
+def main():
+    suma = op.suma(4, 6)
+    res = op.resta(8, 3)
+    mul = op.multipli(4, 6)
+    div = op.divi(24, 4)
+
+    print(suma, res, mul, div)
+
+if __name__ == '__main__':
+    main()
+
+
+
---
+++ operaciones.py
@@ -0,0 +1,14 @@
+def suma(a, b):
+    return a + b
+
+
+def resta(num1, num2):
+    return num1 - num2
+
+
+def multipli(num1, num2):
+    return num1 * num2
+
+
+def divi(num1, num2):
+    return num1 / num2
